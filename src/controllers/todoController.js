e4430ad606e325dbeef7fb0aa3332ddc0f5c86ca
Akashdeep Singh <akashdeepsawna01@gmail.com>
2025-06-25 14:24:21 +0530

    created a todo

---
+++ src/controllers/todoController.js
@@ -0,0 +1,27 @@
+import { Todo } from "../models/todoModel.js";
+
+// create a Todo
+// read a Todo
+// update
+// Delete
+
+// create a Todo
+
+
+export const createTodo = async (req, res) => {
+        try {
+        const { title, description } = req.body;
+        const newTodo = await Todo.create({
+            title,
+            description
+        })
+        res.status(201).json(newTodo);
+    }
+
+ catch (error) {
+    res.status(404).json({ message: error.message })
+}
+}
+
+
+// read a Todo
